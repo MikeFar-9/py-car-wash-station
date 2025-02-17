610366c3bada3f9bb922925fa35e450a21b08a97
Mike Far <mikefarenyuk@gmail.com>
2025-02-17 23:13:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,45 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(self, car: Car) -> float:
+        comfort_factor = car.comfort_class
+        cleanliness_factor = self.clean_power - car.clean_mark
+        distance_factor = self.distance_from_city_center
+        rating_factor = self.average_rating
+        price = (comfort_factor * cleanliness_factor
+                    * rating_factor / distance_factor)
+        return round(price, 1)
+
+    def wash_single_car(self, car: Car) -> None:
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self, rating: float) -> None:
+        self.count_of_ratings += 1
+        total_rating = ((self.count_of_ratings - 1)
+                        * self.average_rating + rating)
+        self.average_rating = round((total_rating / self.count_of_ratings), 1)
+
+    def serve_cars(self, cars: list[Car]) -> float:
+        total_income = 0.0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income = self.calculate_washing_price(car)
+                total_income += income
+                self.wash_single_car(car)
+        return round(total_income, 1)
